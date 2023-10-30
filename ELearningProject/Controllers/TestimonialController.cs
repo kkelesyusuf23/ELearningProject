c1615b779651ce41dede4f4f135a3c0b7e579565
kkelesyusuf <kkelesyusuf23@gmail.com>
2023-10-31 00:32:07 +0300

    Testimonial tamamlandı

---
+++ ELearningProject/Controllers/TestimonialController.cs
@@ -0,0 +1,45 @@
+﻿using ELearningProject.DAL.Context;
+using ELearningProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELearningProject.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        ELearningContext context = new ELearningContext();
+        // GET: Testimonial
+        public ActionResult Index()
+        {
+            ViewBag.testimonialCount = context.Testimonials.Count();
+            var values = context.Testimonials.ToList();
+            return View(values);
+        }
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var value = context.Testimonials.Find(id);
+            context.Testimonials.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var value = context.Testimonials.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            var value = context.Testimonials.Find(testimonial.TestimonialID);
+            value.NameSurname = testimonial.NameSurname;
+            value.Title = testimonial.Title;
+            value.ImageURL = testimonial.ImageURL;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
